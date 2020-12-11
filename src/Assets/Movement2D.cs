eaf3a4dfd2e0798ff6812ac9d26333367491d02a
Jair Bortolucci Júnior <jaibortolucci@gmail.com>
2020-12-11 10:36:55 -0300

    Source

---
+++ src/Assets/Movement2D.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement2D : MonoBehaviour {
+
+    Vector2 targetVelocity;
+    Vector2 currentVelocity;
+    Vector2 velocity;
+
+    public float maxSpeed     = 5;
+    public float acceleration = 100f;
+
+    void Start() {
+
+
+    }
+
+    void Update() {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(horizontalInput, verticalInput).normalized;
+
+        velocity = Vector2.SmoothDamp(velocity, maxSpeed * direction, ref currentVelocity, maxSpeed / acceleration);
+
+        transform.position += (Vector3)velocity * Time.deltaTime;
+
+    }
+
+}
---
+++ src/Assets/Movement3D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement3D : MonoBehaviour {
+
+    Vector3 velocity;
+    Vector3 direction = Vector3.zero;
+    Vector3 currentVelocity;
+
+    public float acceleration = 50;
+    public float maxSpeed     = 10;
+
+
+    void Start() {
+    
+        
+    }
+
+
+    void Update() {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput   = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+
+        velocity = Vector3.SmoothDamp(velocity, direction * maxSpeed, ref currentVelocity, maxSpeed / acceleration);
+
+
+        transform.position += velocity * Time.deltaTime;
+
+    }
+
+}
